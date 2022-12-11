56f36c55816c6c091f8ab1ff73d1d8e526b92e78
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-12-11 11:40:22 +0100

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -162,14 +162,14 @@ const NavBar = ({ mint }) => {
             <Navbar.Content>
             {isConnected ? (
             <button
-              className="w-full rounded-xl bg-gradient-to-b from-pink-600 to-purple-600 font-bold cursor-pointer hover:animate-pulse text-white p-2 m-4"
+              className="w-full rounded-xl bg-gradient-to-b from-pink-600 to-purple-600 font-bold cursor-pointer text-white p-2 m-4"
               id="myBtn"
             >
               {ensName ?? truncateEthAddress(address)}
             </button>
           ) : (
             <button
-              className="w-full rounded-xl bg-gradient-to-b from-pink-600 to-purple-600 font-bold cursor-pointer hover:animate-pulse text-white p-2 m-4"
+              className="w-full rounded-xl bg-gradient-to-b from-pink-600 to-purple-600 font-bold cursor-pointer text-white p-2 m-4"
               id="myBtn"
             >
               Connect Wallet
